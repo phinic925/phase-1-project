a42a6a09531fb4eecb534fd5ac543975e2debfcc
phinic925 <phinicnyangate480@gmail.com>
2022-06-21 18:28:45 +0100

    styles the form

--- js/index.js
+++ js/index.js
@@ -1,8 +1,18 @@
 document.addEventListener("DOMContentLoaded",() =>{
     let form = document.querySelector("form")
     form.addEventListener("submit",(e)=>{
+        //\alert(4)
         e.preventDefault();
+        (e.target['search-input'].value);
+        form.reset();
 
     })
 })
-
+ function fetchData(){
+     fetch("https://www.themealdb.com/api/json/v1/1/filter.php?i=egg")
+        
+  
+     .then(resp => resp.json())
+     .then(data=> console.log(data))
+ }
+ fetchData();
