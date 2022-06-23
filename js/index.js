8949f9989a7867af049d00bf3f79f14035321f14
phinic925 <phinicnyangate480@gmail.com>
2022-06-23 22:12:17 +0100

    add event to the buttons

--- js/index.js
+++ js/index.js
@@ -1,26 +1,49 @@
-document.addEventListener("DOMContentLoaded",() =>{
-    let form = document.querySelector("form")
-    form.addEventListener("submit",(e)=>{
-        //\alert(4)
-        e.preventDefault();
-        (e.target['search-input'].value);
-        form.reset();
+let form = document.querySelector("form")
+let searchBtn = document.getElementById("btn-search")
 
-    })
+document.addEventListener("DOMContentLoaded",() =>{
+    
+    //form.addEventListener("submit",(e)=>{
+        //\alert(4)
+       // e.preventDefault();
+        //renderMeals(e.target['search-input'].value);
+        //form.reset();
+
+  //  })
+  searchBtn.addEventListener("click", getMeals)
+  //alert(4)
 })
- function fetchData(){
-     fetch("https://www.themealdb.com/api/json/v1/1/filter.php?i=egg")
+ 
+   
+ function getMeals(){
+     let searchInput = document.getElementById("search-input").value;
+    
+    
+    
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInput}`)
         
   
-     .then(resp => resp.json())
-     .then(data=> console.log(data))
- }
- fetchData();
- function renderMeals(meals){
-     let mealContainer = document.querySelector("#meals-container")
-     meals.forEach(meal => {
-      const image = document.createElement("img")
-     // image.textContent=meal.   
+    .then(resp => resp.json())
+    .then(data=> {
+        console.log(data)
+
+     let mealContainer = document.querySelector("#results")
+     let data1=""
+     data.meals.forEach(meal => {
+         data1 +=  ` <div data-id="${meal.idMeal}"  class = "meal-item">
+         <div class= "meal-image">
+       
+         <img src="${meal.strMealThumb}" class="img-rounded" id="image"> </div>
+          <div class="meal-name">  
+         <h3> ${meal.strMeal} </h3> 
+          <button type="button" id="button" class= "btn btn-primary"> Get Recipe </button>
+          </div>
+          </div>
+          `
          
      })
- }
+     mealContainer.innerHTML = data1; 
+     })
+    }
+    getMeals();
+
