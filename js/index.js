463cd439fde25b3f6008986216c4213a4e3c2251
phinic925 <phinicnyangate480@gmail.com>
2022-06-26 04:50:20 +0100

    add like button

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
     let form = document.querySelector("form")
     let searchBtn = document.getElementById("btn-search")
     let mealContainer = document.querySelector("#meals-container")
-
     let mealContent = document.getElementById("content")
     let searchRecipe = document.getElementById("recipe-up-btn")
     let searchMeals = document.getElementById("recipe-btn")
     let mealResult = document.getElementById("results")
+    let countLikes = document.getElementById('like-count')
+    let likeButton = document.getElementById('like-button')
+    let integer = 0;
 
 
     document.addEventListener("DOMContentLoaded",() =>{
@@ -21,6 +23,11 @@
         mealContainer.addEventListener('click',getRecipe)
         searchRecipe.addEventListener("click",(e) => {
         mealContent.parentElement.classList.remove('showRecipe');
+        
+        likeButton.addEventListener('click', () => {
+        integer += 1
+        countLikes.innerHTML = `${integer} likes`;
+    })
   
         
 
